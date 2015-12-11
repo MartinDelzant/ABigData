849596c6a73ee57eceb71352560218da7456fd9b
RobinVogel <robvogel0@gmail.com>
2015-12-11 19:00:55 +0100

    Modified GoW, added save_features

---
+++ save_features.py
@@ -0,0 +1,26 @@
+import os
+
+def save_textual_data(data,complete_path):
+    saved_path = os.getcwd()
+    try :
+        os.chdir(complete_path)
+        i = 0
+        for s in data :
+            with open("data"+str(i)+".txt","w") as f :
+                f.write(s)
+            i+=1
+        os.chdir(saved_path)
+        return 0
+    except :
+        return 1
+
+def load_textual_data(complete_path):
+    # Pour charger, faire un sort sur la longueur et puis sur la string
+    data = list()
+    a = list(os.walk(complete_path))
+    list_files = a[0][2]
+    list_files = sorted(list_files, key = lambda item : (len(item),item))
+    for filenames in list_files :
+        with open(complete_path + filenames,"r") as f :
+            data.append(f.read())
+    return data
