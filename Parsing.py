1b3576bc3b6396933a54ebd46458cae45565d560
GCarbs <guillaume.carbajal@gmail.com>
2015-12-13 14:35:06 +0100

    Try parsing on example

--- Parsing.py
+++ Parsing.py
@@ -1,11 +1,18 @@
-import numpy as np
-import pandas as pd
-from fonctions import *
-from nltk.parse.malt import MaltParser
+import os
+from nltk.parse import stanford
+os.environ['STANFORD_PARSER'] = 'C:/Users/Guillaume/Documents/Scolarite/Master Data Sciences/Projets/stanford-parser-full-2014-08-27'
+os.environ['STANFORD_MODELS'] = 'C:/Users/Guillaume/Documents/Scolarite/Master Data Sciences/Projets/stanford-parser-full-2014-08-27'
+os.environ['JAVAHOME'] = 'C:/Program Files (x86)/Java/jdk1.8.0_60/bin'
 
-print("Loading training set")
-data, y = loadTrainSet()
+parser=stanford.StanfordParser(model_path="C:/Users/Guillaume/Documents/Scolarite/Master Data Sciences/Projets/englishPCFG.ser.gz")
+sentences = parser.raw_parse_sents(("Hello, my name is Melroy","What is your name?"))
 
-malt.MaltParser(working_dir="/home/rohith/malt-1.7.2",
-	mco="engmalt.linear-1.7",
-	additional_java_args=['-Xmx512m'])
+print(sentences)
+
+#draw the tree
+for line in sentences:
+	for sentence in line:
+		sentence.draw()
+
+dep_parser=stanford.StanfordDependencyParser(model_path="C:/Users/Guillaume/Documents/Scolarite/Master Data Sciences/Projets/englishPCFG.ser.gz")
+print([parse.tree() for parse in dep_parser.raw_parse("The quick brown fox jumps over the lazy dog.")])
